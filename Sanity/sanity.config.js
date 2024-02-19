b660dc2e78bcd6ed8933fe2763ad6172b1b51802
Alexisrdrgz <rodriguez.alexis0404@gmail.com>
2024-02-19 14:32:04 -0800

    frondend y backend con blog integrado

---
+++ Sanity/sanity.config.js
@@ -0,0 +1,17 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemaTypes'
+export default defineConfig({
+  name: 'default',
+  title: 'ITDigital',
+
+  projectId: 'c0ayx3bq',
+  dataset: 'production',
+
+  plugins: [structureTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
